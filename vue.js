cd43f21c8e23455170e49887f3f9b50a5dd3d698
maheen-y <maheenyassar@gmail.com>
2025-11-24 11:34:54 +0000

    Comments added to vue.js file

--- vue.js
+++ vue.js
@@ -89,8 +89,10 @@ new Vue({
             }
         ],
 
+        // Lessons added to basket are stored in array
         basket: [],
 
+        // Javascript object to store customers personal information
         order: {
             firstName: "",
             surname: "",
@@ -101,14 +103,17 @@ new Vue({
             email: ""
         },
 
+        // Message displayed to confirm the order placed
         orderConfirmationMessage: ""
     },
 
     computed: {
+        // Shows number of lessons added to basket
         lessonsInBasket() {
             return this.basket.length;
         },
 
+        // Regular expressions used to check if name and phone are valid
         isCheckoutFormValid() {
             const nameCheck = /^[a-zA-Z ]+$/;
             const phoneCheck = /^[0-9]+$/;
@@ -124,6 +129,7 @@ new Vue({
             );
         },
 
+        // Used to display sorted lessons in terms of ascending/descending order for the attribute selected
         sortedLessons() {
             return this.lessons.slice().sort((a, b) => {
                 let valA = a[this.sortAttribute];
@@ -142,11 +148,13 @@ new Vue({
     },
 
     methods: {
+        // Allows user to switch between the lessons and checkout page
         displayCheckout() {
             this.showLessons = !this.showLessons;
             this.orderConfirmationMessage = ""; // Clear message when switching pages
         },
 
+        // Used to add lessons to basket and change availability
         addLessonToBasket(lesson) {
             if (lesson.space_available > 0) {
                 lesson.space_available--;
@@ -154,11 +162,13 @@ new Vue({
             }
         },
 
+        // Allows user to remove lesson from basket and changes availability accordingly
         removeLesson(index, lesson) {
             this.basket.splice(index, 1);
             lesson.space_available++;
         },
 
+        // Checks if form is valid, submits orders and displays confirmation message
         submitOrder() {
             if (this.isCheckoutFormValid) {
                 this.orderConfirmationMessage = `Thank you, ${this.order.firstName}! Your order has been submitted.`;
@@ -171,8 +181,12 @@ new Vue({
                     postcode: "",
                     email: ""
                 };
+
+                //Basket becomes empty after order placed
                 this.basket = [];
                 this.showLessons = false; // Keep user on checkout page to show message
+               
+                // Displays error message if checkout form is incorrect
             } else {
                 this.orderConfirmationMessage = "Error occurred, please correct the form details.";
             }
