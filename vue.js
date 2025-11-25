9bc7316f4b96bd0edd939fbf61d1e8c1bb0fc036
maheen-y <maheenyassar@gmail.com>
2025-11-25 12:56:48 +0000

    Fetch functions implemented completely

--- vue.js
+++ vue.js
@@ -1,4 +1,4 @@
-new Vue({
+var app = new Vue({
     el: "#app",
     data: {
         website: "After School Lessons & Activities",
@@ -28,14 +28,16 @@ new Vue({
 
     // Fetch function used to retrieve lessons from backend with GET request
     created(){
-        fetch("http://localhost:3000/lessons").then
-        (res => res.json()).then (json=>{
-            this.lessons = json;
-        })
-        .catch(error => console.log("Lessons could not be loaded", error));
-    }, 
-
-
+        fetch("http://localhost:3000/lessons")
+        .then(function(res){
+            return res.json();
+        }).then(function(json){
+            app.lessons = json;
+        }).catch(function(error){
+            console.log("Failed to retrieve lessons", error);
+        });
+    },
+           
     computed: {
         // Shows number of lessons added to basket
         lessonsInBasket() {
@@ -83,46 +85,84 @@ new Vue({
             this.orderConfirmationMessage = ""; // Clear message when switching pages
         },
 
-        // Used to add lessons to basket and change availability
+        // Used to add lessons to basket 
         addLessonToBasket(lesson) {
             if (lesson.space_available > 0) {
                 lesson.space_available--;
                 this.basket.push(lesson);
 
+                // Fetch function updates lesson availability with PUT request
+                // Response in JSON format
                 fetch("http://localhost:3000/lessons/" + lesson._id, {
                     method: "PUT", 
                     headers: {"Content-Type": "application/json"}, 
-                    body: JSON.stringify({
-                        space_available: lesson.space_available
-                    })
-                }).then(res => res.json()).then(json => {
-                    console.log(json);
-                }).catch(error => console.log("Could not update lesson info", error));
+                    body: JSON.stringify({ space_available: lesson.space_available})
+                }).then(function(res){
+                    return res.json();
+                }).then(function(json){
+                    console.log("Lesson information has been updated", json);
+                }).catch(function(error){
+                    console.log("Unable to update lesson information, please try again", error);
+                });
             }
-        },
+        },   // ✅ FIXED: this bracket was missing!
 
         // Allows user to remove lesson from basket and changes availability accordingly
         removeLesson(index, lesson) {
             this.basket.splice(index, 1);
             lesson.space_available++;
 
-
             // Fetch updates lesson availability with PUT request
-            fetch ("http://localhost:3000/lessons/" + lesson._id, {
+            fetch("http://localhost:3000/lessons/" + lesson._id, {
                 method: "PUT", 
-                headers: {"Content-Type": "application/json"} , 
+                headers: {"Content-Type": "application/json"}, 
                 body: JSON.stringify({
                     space_available: lesson.space_available
                 }) 
-            }).then(res => res.json()).then(json =>{
-                console.log(json);
-            }). catch(error => console.log("Could not update lesson info", error));
+            }).then(function(res){
+                return res.json();
+            }).then(function(json){
+                console.log("Lesson info updated successfully", json);
+            }).catch(function(error){
+                console.log("Unable to update lesson information", error);
+            });
         },
 
         // Checks if form is valid, submits orders and displays confirmation message
         submitOrder() {
             if (this.isCheckoutFormValid) {
-                this.orderConfirmationMessage = `Thank you, ${this.order.firstName}! Your order has been submitted.`;
+                const personalDataForOrder = {
+                    firstName: this.order.firstName, 
+                    surname: this.order.surname,
+                    phone: this.order.phone,
+                    address: this.order.address, 
+                    city: this.order.city,
+                    postcode: this.order.postcode,
+                    email: this.order.email, 
+                    lessons: this.basket.map(item => ({
+                        id: item._id,
+                        subject: item.subject
+                    }))
+                }; 
+
+                // Use fetch function for POST request to save new order
+                fetch("http://localhost:3000/orders", {
+                    method: "POST", 
+                    headers: {
+                        "Content-Type": "application/json"
+                    }, 
+                    body: JSON.stringify(personalDataForOrder)
+                }).then(function(res){
+                    res.json().then(function(json){
+                        console.log("Order successful", json);
+                        alert("Your order has successfully been saved");
+                    });
+                }).catch(function(error){
+                    console.log("Your order has not been saved, please try again", error);
+                });
+
+                this.orderConfirmationMessage = `Thank you, ${ this.order.firstName }! Your order has been submitted.`;
+                
                 this.order = {
                     firstName: "",
                     surname: "",
@@ -133,17 +173,14 @@ new Vue({
                     email: ""
                 };
 
-                //Basket becomes empty after order placed
+                // Basket becomes empty after order placed
                 this.basket = [];
                 this.showLessons = false; // Keep user on checkout page to show message
                
-                // Displays error message if checkout form is incorrect
             } else {
+                // Displays error message if checkout form is incorrect
                 this.orderConfirmationMessage = "Error occurred, please correct the form details.";
             }
         }
     }
 });
-
-
-
